0debc3efee890bf006765079241165e958cd55bc
graph226 <graph226@gmail.com>
2015-10-13 20:09:33 +0900

    modified for true behavior
    modified for getting true answer

--- fibo.ml
+++ fibo.ml
@@ -1,4 +1,5 @@
-let rec fibo n = fib n 1 0
-  and fib n a1 a2 = match n with
-    0 -> 1
-  | n -> fib (n-1) (a1 + a2) a1
+let rec fibo n = fib (n,1,0)
+  and fib (n,a1,a2) = match n with
+    0 -> 0
+  | 1 -> a1
+  | n -> fib (n-1,a1+a2,a1);;
